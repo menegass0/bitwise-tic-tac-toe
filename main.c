9f4f4016b4ec338384a621bbed50bd3a34e5ee1a
menegass0 <victormenegasso@unisantos.br>
2026-01-27 17:13:42 -0300

    inserting value and printing it

--- main.c
+++ main.c
@@ -2,9 +2,7 @@
 
 int main()
 {
-    __uint8_t l1;
-    __uint8_t l2;
-    __uint8_t l3;
+    __uint8_t rows[3] = {0, 0, 0};
     __uint8_t position;
     char leitura[2];
 
@@ -18,28 +16,40 @@ int main()
     printf("choose a row and a column like 21 (2nd row, 1st column): ");
     scanf("%2s", leitura);
 
+    // converting the string into uint8
     sscanf(leitura, "%2hhu", &position);
 
-    printf("linha %d\n", (position / 10));
-    printf("coluna %d\n", (position % 10));
+    if (position / 10 > 3 || position % 10 > 3)
+    {
+        printf("invalid value\n");
 
-    // switch (position / 10)
-    // {
-    // case 1:
-    //     printf("row 1\n");
-    //     break;
+        return 0;
+    }
 
-    // case 2:
-    //     printf("row 2\n");
-    //     break;
+    // shifting 1 which is a binary pair (00000001) (3 << column * 2) -> (00010000) ( 01 mask for player 1)
+    // shifting 3 which is a binary pair (00000011) (3 << column * 2) -> (00110000) ( 11 mask for player 2)
 
-    // case 3:
-    //     printf("row 3\n");
-    //     break;
+    rows[(position / 10) - 1] = (0x01 << ((position % 10) - 1) * 2) | rows[(position % 10) - 1];
 
-    // default:
-    //     break;
-    // }
+    printf("number on rows %d\n", rows[(position / 10) - 1]);
+
+    printf("  1   2   3\n");
+    for (__uint8_t i = 0; i < 5; i++)
+    {
+        if (i % 2 == 0)
+        {
+            printf("%d", (i / 2) + 1);
+            for (__uint8_t j = 0; j < 3; j++)
+            {
+                printf(" %s |", (rows[i / 2] >> (j * 2) & 0x03) == 3 ? "11" : (rows[i / 2] >> (j * 2) & 0x03) == 1 ? "01"
+                                                                                                                   : "00");
+            }
+            printf("\n");
+        }
+    }
+
+    printf("row %d\n", (position / 10));
+    printf("column %d\n", (position % 10));
 
     printf("You entered: %2s\n", leitura);
     printf("value of position: %u\n", position);
